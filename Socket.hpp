c35985f4acc5f08e750847936d796184957a001a
MohamedSafa <mhamadsafa98@gmail.com>
2026-04-12 20:55:01 +0300

    Socket implementation with TCP connection with non blocking mode

---
+++ Socket.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Socket.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/04 21:08:28 by msafa             #+#    #+#             */
+/*   Updated: 2026/04/10 19:55:51 by msafa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
+#include <string>
+
+class Socket
+{
+    public:
+        static int createListenSocket(const std::string& host, int port);
+        static void setNonBlocking(int fd);
+};
+
+#endif
