33dde781a041ea8e6303e59ab179dc8365942699
MohamedSafa <mhamadsafa98@gmail.com>
2026-04-15 15:22:15 +0300

    Request class HTTP parser with ParsState enum and initialization

--- http/Request.cpp
+++ http/Request.cpp
@@ -6,11 +6,14 @@
 /*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 18:32:55 by msafa             #+#    #+#             */
-/*   Updated: 2026/04/13 18:33:20 by msafa            ###   ########.fr       */
+/*   Updated: 2026/04/15 15:07:01 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
-Request::Request() {}
+Request::Request()
+    : _parseState(PARSE_REQUEST_LINE), _contentLength(0), _chunked(false)
+{
+}
 Request::~Request() {}
--- http/Request.hpp
+++ http/Request.hpp
@@ -6,18 +6,43 @@
 /*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 18:31:56 by msafa             #+#    #+#             */
-/*   Updated: 2026/04/13 18:32:41 by msafa            ###   ########.fr       */
+/*   Updated: 2026/04/15 14:54:57 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
+#include <map>
+#include <string>
+
+enum ParseState
+{
+    PARSE_REQUEST_LINE,
+    PARSE_HEADERS,
+    PARSE_BODY,
+    PARSE_COMPLETE,
+    PARSE_ERROR
+};
+
 class Request
 {
     public:
+        std::string _method;
+        std::string _uri;
+        std::string _version;
+        std::map<std::string,std::string> _headers;
+        std::string _body;
+        std::string _queryString;
+        std::string _path;
+        ParseState _parseState;
         Request();
         ~Request();
+        void parse(std::string& chunk);
+    private:
+        std::string _rawBuffer;
+        size_t _contentLength;
+        bool _chunked;
 };
 
 #endif
