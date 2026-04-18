47dcb3e1107ff632e114155502eddf1266954a49
MohamedSafa <mhamadsafa98@gmail.com>
2026-04-18 22:25:47 +0300

    Response Class Setters and method implementation

--- http/Response.cpp
+++ http/Response.cpp
@@ -6,12 +6,54 @@
 /*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 18:33:57 by msafa             #+#    #+#             */
-/*   Updated: 2026/04/13 18:34:16 by msafa            ###   ########.fr       */
+/*   Updated: 2026/04/18 22:10:20 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Response.hpp"
+#include <sstream>
 
-Response::Response() {}
+Response::Response()
+    :_version("HTTP/1.1"), _statusCode(200),_statusMessage("OK")
+{}
 Response::~Response() {}
 
+void Response::setStatusCode(int code)
+{
+    _statusCode = code;
+}
+
+void Response::setHeader(const std::string& key, const std::string& value)
+{
+    _header[key] = value;
+}
+
+void Response::setBody(const std::string& content)
+{
+    _body = content;
+}
+
+std::string Response::build()
+{
+    std::string result;
+
+    std::ostringstream oss;
+    oss << _statusCode;
+    std::string statusCode = oss.str();
+    
+    result += _version 
+                + " " 
+                + statusCode
+                + " "
+                + _statusMessage
+                +"\r\n";
+    std::map<std::string, std::string>::iterator itr;
+    
+    for(itr = _header.begin(); itr != _header.end() ; ++itr)
+    {
+        result += itr->first + ": " + itr->second + "\r\n";
+    }
+    result += "\r\n";
+    result += _body;
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -3,18 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akoaik <akoaik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 00:32:10 by akoaik            #+#    #+#             */
-/*   Updated: 2026/03/18 00:36:45 by akoaik           ###   ########.fr       */
+/*   Updated: 2026/04/18 18:48:24 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "server/Socket.hpp"
+#include "http/Request.hpp"
+#include "http/Response.hpp"
+#include <netinet/in.h>
+#include <unistd.h>
+#include <iostream>
+
 int main(void)
-{   
-    
+{
+    // create listening socket on port 8080
+    int listenFd = Socket::createListenSocket("127.0.0.1", 8080);
+    std::cout << "Server listening on 127.0.0.1:8080" << std::endl;
 
+    while(true)
+    {
+        // wait for one client connection
+        struct sockaddr_in clientAddr;
+        socklen_t addrLen = sizeof(clientAddr);
+        int clientFd = accept(listenFd, (struct sockaddr*)&clientAddr, &addrLen);
 
+        if(clientFd == -1)
+            break;
 
+        // receive data from client
+        char buffer[1024];
+        int bytesRead = recv(clientFd, buffer, sizeof(buffer) - 1, 0);
+        buffer[bytesRead] = '\0';
+
+        Request request;
+        std::string chunk(buffer);
+        request.parse(chunk);
+
+        std::cout << "Method: " << request._method << std::endl;
+        std::cout << "URI: " << request._uri << std::endl;
+        std::cout << "Body: " << request._body << std::endl;
+
+        // Create Response
+        Response response;
+        response.setStatusCode(200);
+        response.setHeader("Content-Type", "text/plain");
+        response.setBody("Hello from WebServ!");
+
+        // Build and send response
+        std::string httpResponse = response.build();
+        send(clientFd, httpResponse.c_str(), httpResponse.length(), 0);
+
+        close(clientFd);
+    }
+
+    close(listenFd);
     return(0);
 }
