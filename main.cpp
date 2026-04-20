fe150d35b9bf85a7ae7c3a0ff13fff6aefb8a834
MohamedSafa <mhamadsafa98@gmail.com>
2026-04-20 21:13:09 +0300

    implement poll-based event loop with concurrent client handlin -Accept multiple clients and track in vector -use poll() to monitor listening socket and all client sockets -Receive and accumulate HTTP request data from clients -Parse complete requests and send responses -Gracefully handle client disconnects and errors

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 00:32:10 by akoaik            #+#    #+#             */
-/*   Updated: 2026/04/19 21:03:32 by msafa            ###   ########.fr       */
+/*   Updated: 2026/04/20 21:12:23 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,58 @@ int main(void)
 
         // call poll
         int pollResult = poll(&fds[0], numFds, -1);
+        if(fds[0].revents & POLLIN)
+        {
+            struct sockaddr_in clientAddr;
+            socklen_t addrLen = sizeof(clientAddr);
+            int clientFd = accept(listenFd,(struct sockaddr* )&clientAddr,&addrLen);
+            if(clientFd != -1)
+            {
+                Client* client = new Client(clientFd);
+                connected_clients.push_back(client);
+            }
+            else
+            {
+                std::cerr << "accept() failed" << std::endl;
+            }
+        }
+       for(size_t i = 0; i < connected_clients.size(); i++)
+       {
+        if(fds[i + 1].revents & POLLIN)
+        {
+            char buffer[1024];
+            ssize_t bytesReceieved = recv(fds[i + 1].fd,buffer,sizeof(buffer) - 1,0);
+            if(bytesReceieved > 0)
+            {
+                buffer[bytesReceieved] = '\0';
+                std::string chunk(buffer);
+                connected_clients[i]->recv_buffer += chunk;
+                connected_clients[i]->request->parse(connected_clients[i]->recv_buffer);
+                if(connected_clients[i]->request->_parseState == PARSE_COMPLETE)
+                {
+                    std::string httpResponse = connected_clients[i]->response->build();
+                    ssize_t bytesSent = send(connected_clients[i]->fd,
+                                            httpResponse.c_str(),
+                                            httpResponse.length(),
+                                            0);
+                    if(bytesSent < 0)
+                    {
+                        std::cerr << "send() failed" << std::endl;
+                        delete connected_clients[i];
+                        connected_clients.erase(connected_clients.begin() + i);
+                        i--;
+                    }
+                }
+            }
+            else if(bytesReceieved == 0 || bytesReceieved == -1)
+            {
+                delete connected_clients[i];
+                connected_clients.erase(connected_clients.begin() + i);
+                i--;
+            }
+        }
+       }
     }
-
 }
 
 // int main(int argc, char **argv)
