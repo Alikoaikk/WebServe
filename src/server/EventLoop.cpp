132db3d1a4beb9e017982c3200dcf7ef4c20dfe2
MohamedSafa <mhamadsafa98@gmail.com>
2026-05-09 19:35:00 +0300

    refactor handleClientData and add HTTP errro handling

--- src/server/EventLoop.cpp
+++ src/server/EventLoop.cpp
@@ -6,7 +6,7 @@
 /*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 22:35:24 by msafa             #+#    #+#             */
-/*   Updated: 2026/05/09 19:08:21 by msafa            ###   ########.fr       */
+/*   Updated: 2026/05/09 19:33:31 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,74 +101,73 @@ static const parse::locConfig* findLocation(const parse::serConfig& config, cons
     return winner;
 }
 
+static void buildErrorResponse(Client* client, int code, const std::string& message)
+{
+    client->response->setStatusCode(code);
+    client->response->setHeader("Content-Type","text/html");
+    client->response->setBody("<html><body><h1>" + message + "</h1></body></html>");
+    client->send_buffer = client->response->build();
+    client->response_ready = true;
+}
+
+static void buildResponse(Client* client)
+{
+    const parse::locConfig* loc = findLocation(*client->serverConfig, client->request->_uri);
+    if(loc == NULL)
+    {
+        buildErrorResponse(client,404,"404 Not Found");
+        return;
+    }
+    const std::vector<std::string>& methods = loc->methods;
+    bool methodAllowed = false;
+    for(size_t j = 0; j < methods.size(); j++)
+    {
+        if(methods[j] == client->request->_method)
+        {
+            methodAllowed = true;
+            break;
+        }
+    }
+    if(!methodAllowed)
+        buildErrorResponse(client,405,"405 Method Not Allowed");
+    else
+    {
+        client->response->setStatusCode(200);
+        client->response->setHeader("Content-Type", "text/plain");
+        client->response->setBody("OK");
+        client->send_buffer = client->response->build();
+        client->response_ready = true;
+    }
+}
+
+static void processClientRequest(Client* client)
+{
+    if(client->request->_parseState == PARSE_COMPLETE)
+        buildResponse(client);
+    else if(client->request->_parseState == PARSE_ERROR)
+        buildErrorResponse(client,400,"400 Bad Request");
+}
+
 void handleClientData(std::vector<Client*>& connected_clients, std::vector<struct pollfd>& fds, size_t serverCount)
 {
-    for (size_t i = 0; i < connected_clients.size(); i++)
+    for(size_t i = 0; i < connected_clients.size(); i++)
     {
-        if (fds[serverCount + i].revents & POLLIN)
+        if(!(fds[serverCount + i].revents & POLLIN))
+            continue;
+        char buffer[1024];
+        ssize_t bytesReceived = recv(fds[serverCount + i].fd, buffer, sizeof(buffer) - 1,0);
+        if(bytesReceived > 0)
         {
-            char buffer[1024];
-            ssize_t bytesReceieved = recv(fds[serverCount + i].fd, buffer, sizeof(buffer) - 1, 0);
-
-            if (bytesReceieved > 0)
-            {
-                connected_clients[i]->last_activity = time(NULL);
-                buffer[bytesReceieved] = '\0';
-                std::string chunk(buffer);
-                connected_clients[i]->recv_buffer += chunk;
-                connected_clients[i]->request->parse(connected_clients[i]->recv_buffer);
-                
-                if (connected_clients[i]->request->_parseState == PARSE_COMPLETE)
-                {
-                    const parse::locConfig* loc = findLocation(*connected_clients[i]->serverConfig,connected_clients[i]->request->_uri);
-                    if(loc == NULL)
-                    {
-                        connected_clients[i]->response->setStatusCode(404);
-                        connected_clients[i]->response->setHeader("Content-Type","text/html");
-                        connected_clients[i]->response->setBody("<html><body><h1>404 Not Found</h1></body></html>");
-                    }
-                    else
-                    {
-                        const std::vector<std::string>& methods = loc->methods;
-                        bool methodAllowed = false;
-                        for(size_t j = 0; j < methods.size(); j++)
-                        {
-                            if(methods[j] == connected_clients[i]->request->_method)
-                            {
-                                methodAllowed = true;
-                                break;
-                            }
-                        }
-                        if(!methodAllowed)
-                        {
-                            connected_clients[i]->response->setStatusCode(405);
-                            connected_clients[i]->response->setHeader("Content-Type","text/html");
-                            connected_clients[i]->response->setBody("<html><body><h1>405 Method Not Allowed</h1></body></html>");          
-                        }
-                        else
-                        {
-                            connected_clients[i]->response->setStatusCode(200);
-                            connected_clients[i]->response->setHeader("Content-Type","text/plain");
-                            connected_clients[i]->response->setBody("OK");
-                        }
-                    }
-                    connected_clients[i]->send_buffer = connected_clients[i]->response->build();
-                    connected_clients[i]->response_ready = true;
-                }
-                else if(connected_clients[i]->request->_parseState == PARSE_ERROR)
-                {
-                    connected_clients[i]->response->setStatusCode(400);
-                    connected_clients[i]->response->setHeader("Content-Type","text/html");
-                    connected_clients[i]->response->setBody("<html><body><h1>400 Bad Request</h1></body></html>");
-                    connected_clients[i]->send_buffer = connected_clients[i]->response->build();
-                    connected_clients[i]->response_ready = true;
-                }
-            }
-            else if (bytesReceieved == 0 || bytesReceieved == -1)
-            {
-                handleClientDisconnect(connected_clients, i);
-                i--;
-            }
+            connected_clients[i]->last_activity = time(NULL);
+            buffer[bytesReceived] = '\0';
+            connected_clients[i]->recv_buffer += std::string(buffer);
+            connected_clients[i]->request->parse(connected_clients[i]->recv_buffer);
+            processClientRequest(connected_clients[i]);
+        }
+        else if(bytesReceived == 0 || bytesReceived == -1)
+        {
+            handleClientDisconnect(connected_clients,i);
+            i--;
         }
     }
 }
