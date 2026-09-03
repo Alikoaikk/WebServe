46ddbc511c0bfbf14d49c73c89365bd131d18361
Ali Koaik <alikoaik004@gmail.com>
2026-09-03 22:40:15 +0300

    post method fixed

--- src/http/methods/POST.cpp
+++ src/http/methods/POST.cpp
@@ -6,7 +6,7 @@
 /*   By: akoaik <akoaik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/24 11:08:16 by akoaik            #+#    #+#             */
-/*   Updated: 2026/08/30 20:17:08 by akoaik           ###   ########.fr       */
+/*   Updated: 2026/09/03 21:57:02 by akoaik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,30 +32,34 @@ Response methods::handlePost(const Request& req, const parse::serConfig& serv)
         return res;
     }
 
-    std::string fullPath = buildPath(req._uri, *location, basePath);
-    if (fullPath.empty())
-    {
-        Response res;
-        res.setStatusCode(400);
-        return res;
-    }
-
     std::string body = req._body;
+    std::string fullPath;
 
+    std::string boundary;
     std::map<std::string, std::string>::const_iterator it = req._headers.find("Content-Type");
     if (it != req._headers.end())
+        boundary = getBoundary(it->second);
+
+    if (!boundary.empty())
     {
-        std::string boundary = getBoundary(it->second);
-        if (!boundary.empty())
+        std::string filename;
+        body = parseMultipart(req._body, boundary, filename);
+        if (body.empty() || filename.empty())
         {
-            std::string filename;
-            body = parseMultipart(req._body, boundary, filename);
-            if (body.empty())
-            {
-                Response res;
-                res.setStatusCode(400);
-                return res;
-            }
+            Response res;
+            res.setStatusCode(400);
+            return res;
+        }
+        fullPath = basePath + "/" + filename;
+    }
+    else
+    {
+        fullPath = buildPath(req._uri, *location, basePath);
+        if (fullPath.empty())
+        {
+            Response res;
+            res.setStatusCode(400);
+            return res;
         }
     }
     return uploadFile(fullPath, body, req._uri);
